b94970d80c09352be160dc632e6d23a5fe862dfe
Chih-Hao Lai <junesnow39@gmail.com>
2026-02-01 12:09:22 +0800

    加入OTA

--- esp32_MAX7219_64x8/esp32_MAX7219_64x8.ino
+++ esp32_MAX7219_64x8/esp32_MAX7219_64x8.ino
@@ -8,6 +8,9 @@
 #include <Bonezegei_DS3231.h>
 #include <PS2Keyboard.h>
 #include <TimeLib.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include "Font_Data.h"
 #include "WebPage.h"
 
@@ -150,6 +153,35 @@ void setup(void){
         PRINT("AP IP address:", IP);
         server.begin();
         dnsServer.start(53, "*", IP);
+
+        // --- OTA 設定開始 ---
+        ArduinoOTA.setHostname("LED-Matrix-Display"); // 設定在網路上的名稱
+        ArduinoOTA.setPassword("1201"); // 設定 OTA 更新密碼
+
+        ArduinoOTA.onStart([]() {
+            String type = (ArduinoOTA.getCommand() == U_FLASH) ? "sketch" : "filesystem";
+            Serial.println("Start updating " + type);
+            // 更新開始時關閉顯示，避免干擾
+            P.displayClear(); 
+        });
+        ArduinoOTA.onEnd([]() {
+            Serial.println("\nEnd");
+        });
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
+        ArduinoOTA.onError([](ota_error_t error) {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR) Serial.println("End Failed");
+        });
+
+        ArduinoOTA.begin();
+        PRINTS("OTA Ready");
+        // --- OTA 設定結束 ---
     }
       
     P.begin(2);
@@ -172,6 +204,7 @@ void setup(void){
 
 void loop(void){
     if (hasRTC) {
+        ArduinoOTA.handle(); // 處理 OTA 請求
         dnsServer.processNextRequest();
         handleWiFi();
     }
